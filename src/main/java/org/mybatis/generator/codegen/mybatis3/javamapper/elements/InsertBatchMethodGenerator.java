f744f46ddca1f8f6564ef7d9f9c1888fa8a890b4
yehongyu <yehongyu@huitongjy.com>
2016-03-30 10:49:26 +0800

    init

---
+++ src/main/java/org/mybatis/generator/codegen/mybatis3/javamapper/elements/InsertBatchMethodGenerator.java
@@ -0,0 +1,77 @@
+/*
+ *  Copyright 2009 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.mybatis.generator.codegen.mybatis3.javamapper.elements;
+
+import org.mybatis.generator.api.dom.java.*;
+import org.mybatis.generator.codegen.mybatis3.javamapper.elements.AbstractJavaMapperMethodGenerator;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * //add by yehongyu(add insertBatch)
+ */
+public class InsertBatchMethodGenerator extends AbstractJavaMapperMethodGenerator {
+
+    boolean isSimple;
+
+    public InsertBatchMethodGenerator(boolean isSimple) {
+        super();
+        this.isSimple = isSimple;
+    }
+
+    @Override
+    public void addInterfaceElements(Interface interfaze) {
+        Set<FullyQualifiedJavaType> importedTypes = new TreeSet<FullyQualifiedJavaType>();
+        Method method = new Method();
+
+        method.setReturnType(FullyQualifiedJavaType.getIntInstance());
+        method.setVisibility(JavaVisibility.PUBLIC);
+        method.setName(introspectedTable.getInsertBatchStatementId());
+
+        FullyQualifiedJavaType parameterType = FullyQualifiedJavaType
+                .getNewListInstance();
+        FullyQualifiedJavaType listType;
+        if (isSimple) {
+            listType = new FullyQualifiedJavaType(
+                    introspectedTable.getBaseRecordType());
+        } else {
+            listType = introspectedTable.getRules()
+                    .calculateAllFieldsClass();
+        }
+
+        importedTypes.add(parameterType);
+        importedTypes.add(listType);
+
+        parameterType.addTypeArgument(listType);
+        method.addParameter(new Parameter(parameterType, "list")); //$NON-NLS-1$
+
+        context.getCommentGenerator().addGeneralMethodComment(method,
+                introspectedTable);
+
+        addMapperAnnotations(interfaze, method);
+
+        if (context.getPlugins().clientInsertMethodGenerated(method, interfaze,
+                introspectedTable)) {
+            interfaze.addImportedTypes(importedTypes);
+            interfaze.addMethod(method);
+        }
+    }
+
+    public void addMapperAnnotations(Interface interfaze, Method method) {
+        return;
+    }
+}
